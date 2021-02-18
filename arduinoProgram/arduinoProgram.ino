77ffe0b46d1ad571c3b9c2dab8862a2c80b67634
DanielFuentes17083 <fue17083@uvg.edu.gt>
2021-02-18 10:23:54 -0600

    Cambios en progra del Micro
    Se hacen cambios y se implementa el uso del puerto serial para el manejo correcto de los motores

--- arduinoProgram/arduinoProgram.ino
+++ arduinoProgram/arduinoProgram.ino
@@ -1,8 +1,8 @@
 #include <Stepper.h>
 #include <Servo.h>
-#define STEPS 2048
+#define STEPS 20
 
-Stepper stepper(STEPS, 13, 12, 8, 7);
+Stepper stepper(STEPS, 8, 13, 7, 12);
 Servo ServoMotor1;
 Servo ServoMotor2;
 Servo ServoMotor3;
@@ -21,14 +21,16 @@ void setup() {
   while(!Serial){
     ;
   }
-  stepper.setSpeed(10);
+  stepper.setSpeed(30);
   ServoMotor1.attach(ServoPin1);
   ServoMotor2.attach(ServoPin2);
   ServoMotor3.attach(ServoPin3);
   ServoMotor1.write(0);
   ServoMotor2.write(0);
   ServoMotor3.write(0);
-  
+  stepper.step(1);
+  pinMode(DCMotorPinL, OUTPUT);
+  pinMode(DCMotorPinR, OUTPUT);
 }
 
 void loop() {
@@ -56,10 +58,24 @@ void loop() {
     }
   }
   while (control == 4){
-//    if (Serial.available() > 0){
-//      action = Serial.read();
-//      ServoMotor1.write(action);
-      break;
-//    }
+    digitalWrite(DCMotorPinR, LOW);
+    digitalWrite(DCMotorPinL, HIGH);
+    delay(100);
+    digitalWrite(DCMotorPinR, HIGH);
+    break;
   }
+  while (control == 5){
+    digitalWrite(DCMotorPinL, LOW);
+    digitalWrite(DCMotorPinR, HIGH);
+    delay(100);
+    digitalWrite(DCMotorPinL, HIGH);
+    break;
+  }
+  if (control == 6){
+    stepper.step(1);
+  }
+  if (control == 7){
+    stepper.step(-1);
+  }
+  
 }
