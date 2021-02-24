416b95e6b2019f284ddae487680180b0f0b44b74
DanielFuentes17083 <fue17083@uvg.edu.gt>
2021-02-23 18:06:21 -0600

    Finalizacion de la GUI
    Se termina la implementacion de la comunicacion entre la GUI y el puerto serial, se hicieron pruebas exitosas y el proyecto es completamente funcional y presentable

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,33 +1,57 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+import serial
+import struct
 
 Window.clearcolor = (.9, .9, .9, 1)
-pos1 = 0
-pos2 = 0
-pos3 = 0
-
 
 class MyWidget(BoxLayout):
-    def baseMove(self, servo1, servo2, servo3 ):
-        print(1)
-        print(servo1)
-        print(2)
-        print(servo2)
-        print(3)
-        print(servo3)
+    while(1):
+        while(1):
+            try:
+                port = input('Enter the number of the port where the Robot Arm is connected: ')
+                port = 'COM'+port
+                break
+            except:
+                print('Enter a valid number')
+        try:
+            ser = serial.Serial(port, baudrate = 9600, timeout = 1500)
+            ser.close()
+            break
+        except:
+            print('Unable to connect with the port')
+    
+    def baseMove(self, servo1, servo2, servo3, port):
+        ser = serial.Serial(port, baudrate = 9600, timeout = 1500)
+        ser.write(struct.pack('>B',1))
+        ser.write(struct.pack('>B',servo1))
+        ser.write(struct.pack('>B',2))
+        ser.write(struct.pack('>B',servo2))
+        ser.write(struct.pack('>B',3))
+        ser.write(struct.pack('>B',servo3))
+        ser.close()
 
-    def dcLeft(self):
-        print('4')
+    def dcLeft(self, port):
+        ser = serial.Serial(port, baudrate = 9600, timeout = 1500)
+        ser.write(struct.pack('>B',4))
+        ser.close()
         
-    def dcRight(self):
-        print('5')
+    def dcRight(self, port):
+        ser = serial.Serial(port, baudrate = 9600, timeout = 1500)
+        ser.write(struct.pack('>B',5))
+        ser.close()
         
-    def steppLeft(self):
-        print('6')
+    def steppLeft(self, port):
+        ser = serial.Serial(port, baudrate = 9600, timeout = 1500)
+        ser.write(struct.pack('>B',6))
+        ser.close()
         
-    def steppRight(self):
-        print('7')
+    def steppRight(self, port):
+        ser = serial.Serial(port, baudrate = 9600, timeout = 1500)
+        ser.write(struct.pack('>B',7))
+        ser.close()
+        
 class Window(App):
     def build(self):
         return MyWidget()
